545b61c9e416a07706f243317e48527a3fca9d4c
DAZUN XING <dazun.xing@gmail.com>
2020-05-16 01:13:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,9 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRANDS=[]
+  #BRANDS_UNIQUE=[]
 
   def initialize(brand)
     @brand = brand
@@ -9,6 +12,11 @@ class Shoe
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+  end
+  
+  def brand=(brand)
+    @brand =brand
+    BRANDS << brand
   end
 
 end

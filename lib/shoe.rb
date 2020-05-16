704d878aaf2b24aaeb9f708c15874f83d6e6a330
DAZUN XING <dazun.xing@gmail.com>
2020-05-16 01:19:45 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,6 +7,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
   def cobble
@@ -14,9 +15,9 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  def brand=(brand)
-    @brand =brand
-    BRANDS << brand
-  end
+  # def brand=(brand)
+  #   @brand =brand
+  #   BRANDS << brand
+  # end
 
 end
